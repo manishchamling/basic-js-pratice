4d3ce79d1da693a73232382f137e0f764d193596
manishrai <unknownms373@gmail.com>
2024-07-18 17:35:43 +0545

    learned about variables

---
+++ variables.js
@@ -0,0 +1,18 @@
+//es5 this is the version of variables 
+var person = "Ram";
+
+//es6
+
+var person = "ram";
+var person = "sita";
+console.log(person);
+// Always starts with const varibales
+const table = "3 table";
+let phone = "9839394839";
+phone = "973949493";
+
+console.log(table, phone);
+// ctrl + / to comment sinle comment
+/* This is
+for multiline 
+comment*/
