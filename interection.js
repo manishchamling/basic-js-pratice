3479ee33062cd5b4c66f48fc53e0457d55114a44
manishrai <unknownms373@gmail.com>
2024-07-18 17:54:23 +0545

    learned about interection

---
+++ interection.js
@@ -0,0 +1,12 @@
+// The types of interection are as follows:
+alert("hellow world");
+
+// promtp
+const age = prompt("How old are you?");
+
+// confirm
+const above18 = confirm("Are you 18 above?");
+console.log(age, above18);
+
+alert(`your are${age} years old`); // string literal
+// Write a js program to create sum calculator using user interection
