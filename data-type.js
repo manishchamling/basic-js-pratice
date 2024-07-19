3eab449f1e9cf369da069fa5e3def04504ae923c
manishrai <unknownms373@gmail.com>
2024-07-19 17:05:31 +0545

    learning datatypes

---
+++ data-type.js
@@ -0,0 +1,12 @@
+const num1 = prompt("Enter your number:");
+const num2 = prompt("Enter second number:");
+const sum = Number(num1) + Number(num2);
+console.log(sum);
+alert(`The number is ${sum}`);
+//There are 5 types of datatypes (primitive datatype)
+// comples datatype
+// type conversion / coercion
+// const a="2"
+// console.log(type of s); // check datatype
+// const num = Number (a); // type conversion 
+// console.log (num);
