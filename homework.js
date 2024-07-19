a63a64df92cbf51fdc2f28a300f5d7ad0552a965
manishrai <unknownms373@gmail.com>
2024-07-19 13:28:53 +0545

    made calculator for sum

---
+++ homework.js
@@ -0,0 +1,13 @@
+// Write a js program to create sum calculator using user interection
+// alert
+alert("Calculator for Sum");
+
+//Using prompt to take a number from the user
+const num1 = parseInt(prompt("Enter the first number:"));
+const num2 = parseInt(prompt("Enter the second number:"));
+
+// sum is the variable is used to find the sum of two number
+const sum = num1 + num2;
+
+//string literal
+alert(`The sum of two number is ${sum}`);
