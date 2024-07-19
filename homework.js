6fdef10691be433f43553bbf4418ab12e4dc60f4
manishrai <unknownms373@gmail.com>
2024-07-19 13:35:25 +0545

    finding sum of three number

--- homework.js
+++ homework.js
@@ -5,9 +5,9 @@ alert("Calculator for Sum");
 //Using prompt to take a number from the user
 const num1 = parseInt(prompt("Enter the first number:"));
 const num2 = parseInt(prompt("Enter the second number:"));
-
+const num3 = parseInt(prompt("Enter the third number:"));
 // sum is the variable is used to find the sum of two number
-const sum = num1 + num2;
+const sum = num1 + num2 + num3;
 
 //string literal
 alert(`The sum of two number is ${sum}`);
