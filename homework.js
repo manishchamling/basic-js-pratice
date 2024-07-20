23e21016945488bca2425cdcfdc84f281fb0d47f
manishrai <unknownms373@gmail.com>
2024-07-20 22:14:02 +0545

    learned about switch

--- homework.js
+++ homework.js
@@ -13,25 +13,52 @@
 // alert(`The sum of two number is ${sum}`);
 
 // write a js program to check the day of the week
+alert("find the day");
+const day = Number(prompt("Enter number 1 to 7 "));
+switch (day) {
+  case 1:
+    alert("Sunday");
+    break;
+  case 2:
+    alert("Monday");
+    break;
+  case 3:
+    alert("Tueday");
+    break;
+  case 4:
+    alert("wednesday");
+    break;
+  case 5:
+    alert("Thursday");
+    break;
+  case 6:
+    alert("Friday");
+    break;
+  case 7:
+    alert("Saturday");
+    break;
+  default:
+    alert("choose number 1 to 7 not other'Thank You' ");
+}
 
 // write a js progaram to check the grade of the student
-alert("Welcome to grade finder");
+// alert("Welcome to grade finder");
 
-const score = Number(prompt("Enter Your score"));
-switch (true) {
-  case score >= 80:
-    alert("Your Grade is A");
+// const score = Number(prompt("Enter Your score"));
+// switch (true) {
+//   case score >= 80:
+//     alert("Your Grade is A");
 
-  case score >= 60 && score <= 80:
-    alert("Your Grade is B");
+//   case score >= 60 && score <= 80:
+//     alert("Your Grade is B");
 
-  case score <= 60:
-    alert("Your are Fail ");
-  default:
-    alert(
-      " you have to put a number or score otherwise you can't find your grade"
-    );
-}
+//   case score <= 60:
+//     alert("Your are Fail ");
+//   default:
+//     alert(
+//       " you have to put a number or score otherwise you can't find your grade"
+//     );
+// }
 
 // const a = "helloworld";
 // console.log(a);
