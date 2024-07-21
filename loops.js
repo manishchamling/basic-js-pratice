42f8c257416ae9b8357dc6a937b5384611e247cf
manishrai <unknownms373@gmail.com>
2024-07-21 17:34:27 +0545

    learned loops

---
+++ loops.js
@@ -0,0 +1,56 @@
+/*
+for 
+ do while
+while
+*/
+
+// for (syntax)
+// for(begin; step condition; increase begin step){
+// code to be executed
+// }
+// write a js program for multiplication table
+const table = Number(prompt("Enter the number for table"));
+if (isNaN(table)) {
+  alert("You didn't enter number");
+}
+for (let i = 1; i <= 10; i++) {
+  console.log(`${table} * ${i} = ${table * i}`);
+}
+
+// do while
+/*begin step
+do{
+code execution
+ increase begin step
+ }
+ while(step condition)
+
+*/
+// while loop
+
+/*
+begin step 
+while(step condition){
+//code execution
+increase begin step
+}
+*/
+const table1 = Number(prompt("Enter the number for table"));
+if (isNaN(table)) {
+  alert("You didn't enter number");
+}
+let a = 1;
+do {
+  console.log(`${table} * ${a} = ${table * a}`);
+  i++;
+} while (a <= 10);
+
+const table3 = Number(prompt("Enter the number for table"));
+if (isNaN(table)) {
+  alert("You didn't enter number");
+}
+let b = 1;
+while (b <= 10) {
+  console.log(`${table} * ${b} = ${table * b}`);
+  b++;
+}
