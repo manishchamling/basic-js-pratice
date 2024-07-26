d8937ad52a6a6645d84ae0786ff455d508ce12ed
manishrai <unknownms373@gmail.com>
2024-07-26 10:28:15 +0545

    conversion of  datatypes

--- data-type.js
+++ data-type.js
@@ -1,12 +1,46 @@
-const num1 = prompt("Enter your number:");
-const num2 = prompt("Enter second number:");
-const sum = Number(num1) + Number(num2);
-console.log(sum);
-alert(`The number is ${sum}`);
-//There are 5 types of datatypes (primitive datatype)
+// const num1 = prompt("Enter your number:");
+// const num2 = prompt("Enter second number:");
+// const sum = Number(num1) + Number(num2);
+// console.log(sum);
+// alert(`The number is ${sum}`);
+// //There are 5 types of datatypes (primitive datatype)
 // comples datatype
 // type conversion / coercion
 // const a="2"
 // console.log(type of s); // check datatype
-// const num = Number (a); // type conversion 
+// const num = Number (a); // type conversion
 // console.log (num);
+// complex datatype
+// 1.object
+// 2.array
+// 3.date
+
+/*
+Type of datatypes in JS
+1.Number => 2 power of 53
+2.String = ""
+3.Boolean = true/false
+4.undefined 
+5.null = standalone value
+6.bigint = it is used when the value of number is high 
+7.object 
+8.symbol
+*/
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// const point = "ab";
+// console.log(typeof point);
+// const newpoint = Number(point);
+// console.log(newpoint);
+
+//type convesion
+//type conversion in boolean
+const isfollowed = "";
+console.log(typeof isfollowed);
+const boolisfollowed = Boolean(isfollowed);
+console.log(typeof boolisfollowed);
+console.log(boolisfollowed);
+// 1 => true 0 => false
+// "anyvalue" => true "blankvalue vayo vane "=> false
