3be34890c1a23026bc477d4ed72dbb8a306291a0
manishrai <unknownms373@gmail.com>
2024-07-29 13:31:09 +0545

    learining reduce method

--- loops.js
+++ loops.js
@@ -141,12 +141,56 @@ const newscore = new Number(30);
 // e.g manish chamling rai => Manish Chamling Rai
 const properCase = (text) => {
   return text //returning the text value
-    .split(" ")  //.split method is using to split the word with the " "(space) When the user put the space in the name that split like (manish chamling rai) here is the two space the split method split this word into three words/
-    .map(  // using map method for loop
-      (word) =>  // 
+    .split(" ") //.split method is using to split the word with the " "(space) When the user put the space in the name that split like (manish chamling rai) here is the two space the split method split this word into three words/
+    .map(
+      // using map method for loop
+      (
+        word //
+      ) =>
         word.slice(0, 1).toUpperCase() + //here we use splice method to divide the word. In the method slice we put the parameter (0, 1) because to differ first letter form other letter and when the first letter differ from other. we put Uppercase string method to convert the word into uppercase or capital letter.
-                word.slice(1, word.length).toLowerCase() //slice is the string method. in this  line we use slice method to slice the word from 1st letter to word.length. and we use another method LowerCase to convert the word into lower case. ([m:0][a:1][n:2][i:3][s:4][h:5]) so we put 0 and 1 parameter to convert first letter into uppercase. we put 1 to word.length parameter to convert other letter except first into LowerCase.
+        word.slice(1, word.length).toLowerCase() //slice is the string method. in this  line we use slice method to slice the word from 1st letter to word.length. and we use another method LowerCase to convert the word into lower case. ([m:0][a:1][n:2][i:3][s:4][h:5]) so we put 0 and 1 parameter to convert first letter into uppercase. we put 1 to word.length parameter to convert other letter except first into LowerCase.
     )
     .join(" "); // .join method is used to join the word. when the word split with the split method at last we have to join that word. so we use join method to join the split word. Things to remember when we split and join the String :[in the split method we split with the (" " space) when we split with the space. we have to join with the (" " space).]
 };
 console.log(properCase("maniSh cHamling rai")); //at last we are calling the properCase function and printing the value.
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+
+//Get the total mass of all character using reduced method
+const initialValue = 0; //declaring the initailvalue
+const sumOfMass = (arr) => {
+  //make the function "name sumOfMass" and put the "arr" parameter in the function
+  return arr.reduce((preval, currval) => {
+    //use reduce method pass the parameter" preval" and "currval" and reutrn arr
+    return preval + Number(currval?.mass); //return the preval and add with tha currval
+  }, initialValue); //We already declare the inital value in the top [const initialValue = 0;]
+};
+console.log(sumOfMass(characters)); // calling function "sumOfMass" and printing the character length
